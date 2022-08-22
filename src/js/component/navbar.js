9192967cad7f92beefdc1a10eabf01853cca3b48
NicolasMC94 <mcnicolas.94@gmail.com>
2022-08-22 19:33:57 +0000

    Inicio de la parte visual

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,14 +3,20 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-dark bg-dark bg-opacity-50 mb-4">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<div className="ms-2">
+					<img src="http://1000marcas.net/wp-content/uploads/2019/12/Star-Wars-Logo-5.png" height={75} />
+				</div>				
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			
+			<div className="dropdown me-3">
+					<button className="dropdown-toggle btn btn-primary" type="button" id="favoritesButton" data-bs-toggle="dropdown" aria-expanded="false">
+						Favorites
+					</button>
+					<ul className="dropdown-menu me-3">
+						<p>Hey</p>
+					</ul>
 			</div>
 		</nav>
 	);
